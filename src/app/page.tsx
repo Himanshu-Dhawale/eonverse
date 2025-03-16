dabcd6972bce646317be7b98c2b359f85798e113
chanchal16 <rajputchanchal17@gmail.com>
2025-03-16 11:18:57 +0530

    update: add sample code to check theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
 export default function Home() {
-  return <></>
+  return (
+    <div className="p-6 flex flex-col items-center justify-center gap-4">
+      <h1 className="text-primary text-5xl">Demo</h1>
+      <button className="bg-secondary text-accent-light py-2 rounded-md px-4 hover:bg-secondary-dark">
+        Click
+      </button>
+    </div>
+  )
 }
