8de3536c3636f470f58986799a5ed3273e1b3263
chanchal16 <rajputchanchal17@gmail.com>
2025-03-19 15:11:38 +0530

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import Footer from '@/components/Footer'
+
 export default function Home() {
   return (
-    <div className="p-6 flex flex-col items-center font-opensauce justify-center gap-4">
+    <div className="py-6 flex flex-col items-center font-opensauce justify-center gap-4">
       <h1 className="text-primary text-5xl font-bold">Demo</h1>
       <button className="bg-secondary text-accent-light py-2 rounded-md px-4 hover:bg-secondary-dark">
         Click
       </button>
+      <Footer />
     </div>
   )
 }
